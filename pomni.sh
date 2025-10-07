7e1d5d1abbd74e5e7cf4e0b7ed60eb276909519b
pm <trey5040@gmail.com>
2025-10-06 21:00:48 -0400

    Update pomni.sh

--- pomni.sh
+++ pomni.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
 progsfile="https://raw.githubusercontent.com/pm-lack/pomni/refs/heads/main/progs.csv"
@@ -7,13 +7,13 @@ progsfile="https://raw.githubusercontent.com/pm-lack/pomni/refs/heads/main/progs
 tmpcsv=$(mktemp)
 curl -Ls "$progsfile" | sed '/^#/d;/^$/d' > "$tmpcsv"
 
-# Build array of options for whiptail
+# Build options array for whiptail
 options_array=()
 while IFS=, read -r tag program comment; do
-    [ -z "$program" ] && continue
-    if [ "$tag" = "A" ] || [ -z "$tag" ]; then
-        # Truncate description to 40 chars to avoid wrapping
-        desc=$(echo "$comment" | sed 's/"/\\"/g' | cut -c1-40)
+    [[ -z "$program" ]] && continue
+    if [[ "$tag" == "A" || -z "$tag" ]]; then
+        # Shorten description
+        desc=$(echo "$comment" | cut -c1-40)
         options_array+=("$program" "$desc" "OFF")
     fi
 done < "$tmpcsv"
@@ -21,4 +21,12 @@ done < "$tmpcsv"
 # Interactive checklist
 chosen=$(whiptail --title "Choose Browsers to Install" \
     --checklist "Select the browsers you want to install:" 20 78 12 \
-    "${options_array[@]}" 3_
+    "${options_array[@]}" 3>&1 1>&2 2>&3) || exit 1
+
+# Clean up
+rm "$tmpcsv"
+
+# Remove quotes
+chosen=$(echo "$chosen" | tr -d '"')
+
+echo "You selected: $chosen"
